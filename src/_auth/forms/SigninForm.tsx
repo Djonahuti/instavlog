e9adeda8a3ca5a1f72e5a0adcb3fd9c567af4cef
Djonahuti <djonah04@gmail.com>
2025-02-17 19:56:08 +0100

    commit

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -108,7 +108,7 @@ const SigninForm = () => {
             )}
           </Button>
 
-          <p className="text-small-regular text-light-2 text-center mt-2">
+          <p className="text-small-regular text-center mt-2">
             Don&apos;t have an account?
             <Link
               to="/sign-up"
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -158,7 +158,7 @@ const SignupForm = () => {
             )}
           </Button>
 
-          <p className="text-small-regular text-light-2 text-center mt-2">
+          <p className="text-small-regular text-center mt-2">
             Already have an account?
             <Link
               to="/sign-in"
