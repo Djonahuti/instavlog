ce278739edd60d229833ffd8e8d0e4fae828abf0
Djonahuti <djonah04@gmail.com>
2025-05-17 12:33:47 +0100

    commit

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -17,7 +17,7 @@ export function ModeToggle() {
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
           <Sun className="h-[2.2rem] w-[2.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-yellow-300" />
-          <Moon className="absolute h-[2.2rem] w-[2.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Moon className="absolute h-[2.2rem] w-[2.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-blue-700" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
